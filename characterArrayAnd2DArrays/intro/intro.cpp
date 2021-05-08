3aa0c2bb6dc0f5cec2cb1931b59bf32012cbcb22
Rohan <rohansharma.08062001@gmail.com>
2021-05-08 19:34:58 +0530

    bit of experiment with functions

--- characterArrayAnd2DArrays/intro/intro.cpp
+++ characterArrayAnd2DArrays/intro/intro.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int lengthOfString(char str[]) {
+	int count = 0;
+	for(int i = 0; str[i] != '\0'; i ++) {
+		count ++;
+	}
+return count;
+}
+
 int main() {
 	char name[100];
 	cout << "Please enter your name: ";
 	cin >> name;
-	name[1] = '\0';
 	cout << "Your name is: " << name << endl;
+	cout << "Length of string is: " << lengthOfString(name) << endl;
 return 0;
 }
