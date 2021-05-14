1dd7300cca14526f958dc304dd6577f97343ca2d
Rohan <rohansharma.08062001@gmail.com>
2021-05-14 20:44:58 +0530

    pointer arithemetics

---
+++ pointers/pointerArithematic/pointerArithematic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 10;
+	int * p = &i;
+	cout << p << endl;
+	p = p + 2;
+	cout << p << endl;
+	p = p - 2;
+	cout << p << endl;
+return 0;
+}
