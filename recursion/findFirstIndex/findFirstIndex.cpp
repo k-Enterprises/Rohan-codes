cd3b87639c615e901cefc05d311ab22836809ec3
Rohan <rohansharma.08062001@gmail.com>
2021-05-16 23:03:43 +0530

    find index of element using recursion

---
+++ recursion/findFirstIndex/findFirstIndex.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void takeInput(int arr[], int n) {
+	for(int i = 0; i < n; i ++) {
+		cout << "Enter the value of element " << i << ": ";
+		cin >> arr[i];
+	}
+return;
+}
+
+int findX(int arr[], int n, int x) {
+	if(n == 0) {
+		return -1;
+	}
+	if(arr[0] == x) {
+		return 0;
+	}
+	int ans = findX(arr + 1, n - 1, x);
+	if(ans == -1) {
+		return -1;
+	}else{
+		return ans + 1;
+	}
+}
+
+int main() {
+	int arr[100], n, x;
+	cout << "Enter the size of the array: ";
+	cin >> n;
+	takeInput(arr, n);
+	cout << "Enter the element you want to find: ";
+	cin >> x;
+	cout << "The element is at: "  << findX(arr, n, x) << endl;
+return 0;
+}
