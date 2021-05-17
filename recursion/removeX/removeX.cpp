8083736d977bc8a547d9762927a1bf1730ca18c8
Rohan <rohansharma.08062001@gmail.com>
2021-05-17 20:09:09 +0530

    remove x from string using recursion

---
+++ recursion/removeX/removeX.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void removeX(char str[]) {
+	if(strlen(str) == 0) {
+		return;
+	}
+	if(str[0] == 'x') {
+		for(int i = 0; i < strlen(str); i ++) {
+			str[i] = str[i + 1];
+		}
+	}
+	removeX(str + 1);
+}
+
+int main() {
+	char str[100];
+	cout << "Enter the string: ";
+	cin >> str;
+	removeX(str);
+	cout << str << endl;
+return 0;
+}
