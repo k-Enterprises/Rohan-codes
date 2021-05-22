c77553a8cc4ec8b1c9d98064118c145b60965819
Rohan <rohansharma.08062001@gmail.com>
2021-05-23 00:26:47 +0530

    initialization list

---
+++ Oop/classes/initialisationList/Class.cpp
@@ -0,0 +1,9 @@
+class Student {
+	private :
+	int age;
+	int const rollNumber;
+	public :
+	Student(int r, int d): rollNumber(r), age(d) {
+		
+	}
+};
---
+++ Oop/classes/initialisationList/initialisationList.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+#include "Class.cpp"
+
+int main() {
+	Student s1(20, 21);
+return 0;
+}
