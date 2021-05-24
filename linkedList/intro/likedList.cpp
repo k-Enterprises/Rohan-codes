3100a5c6b38defc5acb9d6f3796832da7d4fb2ba
Rohan <rohansharma.08062001@gmail.com>
2021-05-24 10:49:01 +0530

    Linked list intro

---
+++ linkedList/intro/likedList.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+#include "Node.cpp"
+
+void printList(Node * head) {
+	Node * temp = head; // always create a temp in case of linked list so that we don't loose head
+	while(temp != NULL) {
+		cout << temp -> data << " ";
+		temp = temp -> next;
+	}
+	cout << endl;
+}
+
+int main() {
+	// statically 
+	Node n1(1);
+	Node * head = & n1;
+	Node n2(2);
+	Node n3(3);
+	Node n4(4);
+	Node n5(5);
+	n1.next = & n2;
+	n2.next = & n3;
+	n3.next = & n4;
+	n4.next = & n5;
+	cout << "Linked List 1: ";
+	printList(head);
+	/*cout << n1.data << endl;
+	cout << n2.data << endl;
+	cout << "Head: " <<head << endl;
+	cout << "Address of n1: " <<&n1 << endl;
+	cout << head -> data << endl;*/
+
+	//dynamically 
+	Node * n6 = new Node(6);
+	Node * head2 = n6;
+	Node * n7 = new Node(7);
+	Node * n8 = new Node(8);
+	Node * n9 = new Node(9);
+	Node * n10 = new Node(10);
+	n6 -> next = n7;
+	n7 -> next = n8;
+	n8 -> next = n9;
+	n9 -> next = n10;
+	cout << "Dynamic Node Linked list: ";
+	printList(head2);
+return 0;
+}
