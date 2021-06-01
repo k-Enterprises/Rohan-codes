28ed04049c197fe5f7b79df3efc6711ecf5bed4f
Rohan <rohansharma.08062001@gmail.com>
2021-06-02 00:35:58 +0530

    Inorder traversal printing in binary trees

---
+++ binaryTrees/inOrder/inOrder.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+#include "BinaryTreeNode.h"
+using namespace std;
+
+BinaryTreeNode<int> * takeInput() {
+	int rootData;
+	cout << "Enter the root data: ";
+	cin >> rootData;
+	if(rootData == -1) {
+		return NULL;
+	}
+	BinaryTreeNode<int> * root = new BinaryTreeNode<int>(rootData);
+	queue<BinaryTreeNode<int>*> pendingNodes;
+	pendingNodes.push(root);
+	while(pendingNodes.size() != 0) {
+		BinaryTreeNode<int> * front = pendingNodes.front();
+		pendingNodes.pop();
+		int leftData, rightData;
+		cout << "Enter the value of left child of " << front -> data << ": ";
+		cin >> leftData;
+		if(leftData != -1) {
+			BinaryTreeNode<int> * leftChild = new BinaryTreeNode<int>(leftData);
+			front -> left = leftChild;
+			pendingNodes.push(leftChild);
+		}
+		cout << "Enter the value of right child of " << front -> data << ": ";
+		cin >> rightData;
+		if(rightData != -1) {
+			BinaryTreeNode<int> * rightChild = new BinaryTreeNode<int>(rightData);
+			front -> right = rightChild;
+			pendingNodes.push(rightChild);
+		}
+	}
+return root;
+}
+
+void inOrder(BinaryTreeNode<int> * root) {
+	if(root == NULL) {
+		return;
+	}
+	inOrder(root -> left);
+	cout << root -> data << " ";
+	inOrder(root -> right);
+return;
+}
+
+int main() {
+	BinaryTreeNode<int> * root = takeInput();
+	inOrder(root);
+	cout << endl;
+	delete root;
+return 0;
+}
