c1e2097bc70ea0823d6f09ef36f2588b50bcbc02
Rohan <rohansharma.08062001@gmail.com>
2021-06-03 20:54:17 +0530

    Return height of binary tree

---
+++ binaryTrees/heightOfBinaryTree/heightOfBinaryTree.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <queue>
+#include "BinaryTreeNode.h"
+using namespace std;
+
+BinaryTreeNode<int> * takeInput() {
+	int rootData;
+	cout << "Enter the root data: ";
+	cin >> rootData;
+	if(rootData == -1) {
+		return NULL;
+	}
+	BinaryTreeNode<int> * root = new BinaryTreeNode<int>(rootData);
+	queue<BinaryTreeNode<int>*> pendingNodes;
+	pendingNodes.push(root);
+	while(pendingNodes.size() != 0) {
+		BinaryTreeNode<int> * front = pendingNodes.front();
+		pendingNodes.pop();
+		int leftData, rightData;
+		cout << "Enter the value of left child of " << front -> data << ": ";
+		cin >> leftData;
+		if(leftData != -1) {
+			BinaryTreeNode<int> * leftChild = new BinaryTreeNode<int>(leftData);
+			front -> left = leftChild;
+			pendingNodes.push(leftChild);
+		}
+		cout << "Enter the value of right child of " << front -> data << ": ";
+		cin >> rightData;
+		if(rightData != -1) {
+			BinaryTreeNode<int> * rightChild = new BinaryTreeNode<int>(rightData);
+			front -> right = rightChild;
+			pendingNodes.push(rightChild);
+		}
+	}
+return root;
+}
+
+int heightOfBinaryTree(BinaryTreeNode<int> * root) {
+	if(root == NULL) {
+		return 0;
+	}
+	int leftHeight = heightOfBinaryTree(root -> left);
+	int rightHeight = heightOfBinaryTree(root -> right);
+return 1 + max(leftHeight, rightHeight);
+}
+
+int main() {
+	BinaryTreeNode<int> * root = takeInput();
+	int height = heightOfBinaryTree(root);
+	cout << endl;
+	cout << "Height of the binary tree is: " << height << endl;
+return 0;
+}
